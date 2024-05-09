5729bdc718bfbef9299bbafcc66d11a6acbfd124
Vishal Singh <73434540+TechnicalVishalJi@users.noreply.github.com>
2024-05-09 09:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-from g4f.gui import run_gui
-run_gui()
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
