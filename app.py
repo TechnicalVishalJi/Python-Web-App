69c855cb4c494471630232c68cd9c65c4fec9e3b
Vishal Singh <73434540+TechnicalVishalJi@users.noreply.github.com>
2024-07-08 05:41:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ from flask_cors import CORS
 import uuid
 from g4f.cookies import set_cookies
 import requests
+from selenium.webdriver.chrome.options import Options as ChromeOptions  # Import for Chrome options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import traceback
 
 app = Flask(__name__)
 
